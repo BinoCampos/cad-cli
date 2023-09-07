cd43fd76a7d8736435f297c89eaa48593fdb7272
BinoCampos <fabianocamp@gmail.com>
2023-09-07 21:31:17 +0100

    Refactor

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+u
+Ecadcli.api/src/main/java/com/fcampos/cadcli/domain/model/Cliente.java,9\4\94c238ace369f554b1db5768370b914c18a97320
+B
+cadcli.api/pom.xml,f\a\faa8baa6212929c1b081b4b144c5e255ff94a7cf
+y
+Icadcli.api/src/test/java/com/fcampos/cadcli/domain/model/ClienteTest.java,e\6\e62947cbe414d55c3eb3611c3a8194990bbe88c1
+u
+Ecadcli.api/src/main/java/com/fcampos/cadcli/config/docker-compose.yml,e\e\eea52fed685b16bf4a288029874bde11e69f224f
--- cadcli.api/src/test/java/com/fcampos/cadcli/domain/model/ClienteTest.java
+++ cadcli.api/src/test/java/com/fcampos/cadcli/domain/model/ClienteTest.java
@@ -1,18 +1,30 @@
 package com.fcampos.cadcli.domain.model;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class ClienteTest {
 
+	@Mock
+	Cliente clienteMock;
+
 	@Test
-	@DisplayName("Nome não pode ser nulo")
-	void nomeDeClienteNulo() {
-		Cliente cliente = new Cliente();
-		cliente.setNome(null);
-		assertTrue(false);
+	void nomeDeClienteNaoDeveSerNulo() {
+
+		when(clienteMock.getNome()).thenReturn("john");
+
+		String nome = clienteMock.getNome();
+
+		assertNotNull(nome);
+
 	}
 
 }
